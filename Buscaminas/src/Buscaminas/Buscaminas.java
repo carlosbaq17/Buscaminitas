d53aae59450aed65ebeb9915cedc6df87d7be49f
carlosbaq17 <carlosbaq24@gmail.com>
2022-01-31 13:50:55 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Buscaminas/src/Buscaminas/Buscaminas.java
+++ Buscaminas/src/Buscaminas/Buscaminas.java
@@ -4,7 +4,7 @@ public class Buscaminas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+sdasdasd
 	}
 
 }
--- Buscaminas/src/module-info.java
+++ Buscaminas/src/module-info.java
@@ -1,2 +1,3 @@
 module Buscaminas {
+	exports Buscaminas;
 }
